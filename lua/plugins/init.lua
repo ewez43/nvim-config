f8ee31c25096aa92cdb265b0f1f05692aaaed724
ewez <piorek.ewa@wp.pl>
2026-03-18 16:17:26 +0100

    Added syntax highlighting for tcl with nvim-lint

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -46,4 +46,64 @@ return {
   {
     "tpope/vim-rails",
   },
+  {
+    "nat-418/tcl.nvim",
+    ft = "tcl",
+    dependencies = {
+      "mfussenegger/nvim-lint",
+    },
+    config = function()
+      require("tcl").setup()
+    end,
+  },
+  {
+    "mfussenegger/nvim-lint",
+    config = function()
+      local lint = require "lint"
+
+      lint.linters.nagelfar = {
+        cmd = "tclsh",
+        stdin = false,
+        args = {
+          vim.fn.expand "~/.local/share/nagelfar/nagelfar.tcl",
+          "-s", -- suppress some noise (optional)
+        },
+        stream = "stdout",
+        ignore_exitcode = true,
+        parser = function(output, bufnr)
+          local diagnostics = {}
+          local lines = vim.split(output, "\n")
+
+          for _, line in ipairs(lines) do
+            -- Nagelfar output example:
+            -- file.tcl:12: warning: something
+            local filename, lnum, type, msg = line:match "([^:]+):(%d+): (%w+): (.+)"
+
+            if filename and lnum then
+              table.insert(diagnostics, {
+                lnum = tonumber(lnum) - 1,
+                col = 0,
+                message = msg,
+                severity = type == "error" and vim.diagnostic.severity.ERROR or vim.diagnostic.severity.WARN,
+                source = "nagelfar",
+              })
+            end
+          end
+
+          return diagnostics
+        end,
+      }
+
+      lint.linters_by_ft = {
+        tcl = { "nagelfar" },
+      }
+
+      -- auto-run on events
+      vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter" }, {
+        callback = function()
+          require("lint").try_lint()
+        end,
+      })
+    end,
+  },
 }
