17cf017dacc3e649140c0dac2fa5d2b45e247860
ewez <piorek.ewa@wp.pl>
2026-04-15 11:03:12 +0200

    Fixed TypeScript LSP, now properly shows inline comments

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,7 +1,26 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "clangd", "ruby_lsp", "tsserver", "pylsp",
-  "gopls", "lua_ls" }
+vim.lsp.config("ts_ls", {
+  settings = {
+    typescript = {
+      suggest = {
+        autoImports = true,
+      },
+      inlayHints = {
+        includeInlayParameterNameHints = "all",
+        includeInlayFunctionParameterTypeHints = true,
+        includeInlayVariableTypeHints = true,
+      },
+    },
+    javascript = {
+      suggest = {
+        autoImports = true,
+      },
+    },
+  },
+})
+
+local servers = { "html", "cssls", "clangd", "ruby_lsp", "ts_ls", "pylsp", "gopls", "lua_ls" }
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- read :h vim.lsp.config for changing options of lsp servers
